3f7c2da0117aa38667f3bedb4f5e4d6313c2eaa9
Necromancerlvl69 <krupaldudhat020406@gmail.com>
2022-11-15 23:44:11 -0500

    my_profile view added

--- Downloads/new/NEWSAPPLICATION/NEWZTAA/users/views.py
+++ Downloads/new/NEWSAPPLICATION/NEWZTAA/users/views.py
@@ -246,3 +246,7 @@ def register(request):
     else:
         return render(request, 'users/signup.html')         
 
+def my_profile(request):
+    profile = Profile.objects.get(user=request.user)
+    context = {'profile':profile}
+    return render(request,'profile.html',context)
