99410572632b15b432d07fbf667aed704db3562d
Necromancerlvl69 <krupaldudhat020406@gmail.com>
2022-12-07 19:08:25 -0500

    follow function

--- Downloads/new/NEWSAPPLICATION/NEWZTAA/users/views.py
+++ Downloads/new/NEWSAPPLICATION/NEWZTAA/users/views.py
@@ -148,6 +148,22 @@ def index(request):
     context = {'mylist':listofnews}
     return render(request,'users/home.html',context)
 
+@login_required
+def follow(request):
+    if request.method == 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+user)
+    else:
+        return redirect('/')
 
 # def settings(request):
 
